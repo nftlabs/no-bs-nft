f00879cbe159ae586f77d7785c06c7be458156f0
nkrishang <kn1457@nyu.edu>
2021-04-26 20:28:41 -0400

    fixed padding

--- components/app/home.tsx
+++ components/app/home.tsx
@@ -18,73 +18,75 @@ export default function Home(): JSX.Element {
 
   return (
     <>
-      <ContentWrapper>
-        <Center mt="16">
-          <Stack width="full" spacing="8">
-            <div className="flex justify-between items-baseline">
-              <p className="text-4xl text-gray-800 font-black">
-                Your NFTs
-              </p>
-
-              <SettingsIcon />
-            </div>
-
-            <Stack>
-              <p className="text-center text-lg mb-4">
-                Looks like you own no NFTs!
-              </p>
-              
-              <Center>
-                <button className="border-2 border-black bg-white shadow-md rounded-full h-10 w-64">
-                  Create an NFT
-                </button>
-              </Center>
-
-              <p className="text-center text-lg">
-                or
-              </p>
-
-              <Center>
-                <button className="border-2 border-black bg-white shadow-md rounded-full h-10 w-64">
-                  Buy an NFT
-                </button>
-              </Center>
-
-            </Stack>
-          </Stack>
-        </Center>
-
-        <Center mt="8" mb="16">
-          <Stack width="full" spacing="8">
-            <p className="text-4xl text-gray-800 font-black">
-              Collections
-            </p>           
-          
-
-            <Stack>
-              <p className="text-center text-lg mb-4">
-                {`You haven't saved any collections!`}
+      <div className="mb-32">
+        <ContentWrapper>
+          <Center mt="16">
+            <Stack width="full" spacing="8">
+              <div className="flex justify-between items-baseline">
+                <p className="text-4xl text-gray-800 font-black">
+                  Your NFTs
                 </p>
-              
-              <Center>
-                <button className="border-2 border-black bg-white shadow-md rounded-full h-10 w-64">
-                  Create an NFT collection
-                </button>
-              </Center>
 
-              <p className="text-center text-lg">
-                or
-              </p>
+                <SettingsIcon />
+              </div>
 
-              <Center>
-                <button className="border-2 border-black bg-white shadow-md rounded-full h-10 w-64">
-                  Explore NFT collections
-                </button>
-              </Center>
+              <Stack>
+                <p className="text-center text-lg mb-4">
+                  Looks like you own no NFTs!
+                </p>
+                
+                <Center>
+                  <button className="border-2 border-black bg-white shadow-md rounded-full h-10 w-64">
+                    Create an NFT
+                  </button>
+                </Center>
+
+                <p className="text-center text-lg">
+                  or
+                </p>
+
+                <Center>
+                  <button className="border-2 border-black bg-white shadow-md rounded-full h-10 w-64">
+                    Buy an NFT
+                  </button>
+                </Center>
+
+              </Stack>
             </Stack>
-          </Stack>
-        </Center>
-      </ContentWrapper>
+          </Center>
+
+          <Center mt="8">
+            <Stack width="full" spacing="8">
+              <p className="text-4xl text-gray-800 font-black">
+                Collections
+              </p>           
+            
+
+              <Stack>
+                <p className="text-center text-lg mb-4">
+                  {`You haven't saved any collections!`}
+                  </p>
+                
+                <Center>
+                  <button className="border-2 border-black bg-white shadow-md rounded-full h-10 w-64">
+                    Create an NFT collection
+                  </button>
+                </Center>
+
+                <p className="text-center text-lg">
+                  or
+                </p>
+
+                <Center>
+                  <button className="border-2 border-black bg-white shadow-md rounded-full h-10 w-64">
+                    Explore NFT collections
+                  </button>
+                </Center>
+              </Stack>
+            </Stack>
+          </Center>
+        </ContentWrapper>
+      </div>
     </>
   )
 }
