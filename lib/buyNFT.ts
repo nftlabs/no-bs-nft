77e5b2bc782cd8d609ef67032491df80ed9ce79b
nkrishang <kn1457@nyu.edu>
2021-05-03 18:24:57 -0400

    reformat lib files

--- lib/buyNFT.ts
+++ lib/buyNFT.ts
@@ -1,16 +1,15 @@
 import ethers from 'ethers';
 
 export default async function buyNFT(contract: any, tokenId: number, priceInEther: string) {
+    const price = ethers.utils.parseEther(priceInEther);
 
-  const price = ethers.utils.parseEther(priceInEther);
+    try {
+        const tx = await contract.makeBid(tokenId, price);
+        // Wait for tx to confirm
+        await tx.wait();
 
-  try {
-    const tx = await contract.makeBid(tokenId, price);
-    // Wait for tx to confirm
-    await tx.wait();
-
-    return tx;
-  } catch(err) {
-    console.error(err)
-  }
+        return tx;
+    } catch (err) {
+        console.error(err);
+    }
 }
--- lib/setSaleStatus.ts
+++ lib/setSaleStatus.ts
@@ -1,40 +1,46 @@
 import ethers from 'ethers';
 
 interface TransactionParams {
-  gasLimit: number;
-  txNonce: number;
-  gasPrice: string;
+    gasLimit: number;
+    txNonce: number;
+    gasPrice: string;
 }
 
+export const putNFTOnSale = async (
+    contract: any,
+    tokenId: number,
+    priceInEther: string,
+    txParams: TransactionParams,
+) => {
+    const active = true;
+    const price = ethers.utils.parseEther(priceInEther);
 
-export const putNFTOnSale = async (contract: any, tokenId: number, priceInEther: string, txParams: TransactionParams ) => {
-  
-  const active: boolean = true;
-  const price = ethers.utils.parseEther(priceInEther);
+    try {
+        const tx = await contract.setThreshold(tokenId, price, active, { ...txParams });
+        // wait for the tx to be confirmed
+        await tx.wait();
 
-  try {
-    const tx = await contract.setThreshold(tokenId, price, active, {...txParams});
-    // wait for the tx to be confirmed    
-    await tx.wait()
+        return tx;
+    } catch (err) {
+        console.error(err);
+    }
+};
 
-    return tx
-  } catch(err) {
-    console.error(err)
-  }
-}
+export const takeNFTOffSale = async (
+    contract: any,
+    tokenId: number,
+    txParams: TransactionParams,
+) => {
+    const active = false;
+    const price = ethers.utils.parseEther('0');
 
-export const takeNFTOffSale = async (contract: any, tokenId: number, txParams: TransactionParams ) => {
-  
-  const active: boolean = false;
-  const price = ethers.utils.parseEther('0');
+    try {
+        const tx = await contract.setThreshold(tokenId, price, active, { ...txParams });
+        // wait for the tx to be confirmed
+        await tx.wait();
 
-  try {
-    const tx = await contract.setThreshold(tokenId, price, active, {...txParams});
-    // wait for the tx to be confirmed    
-    await tx.wait()
-
-    return tx
-  } catch(err) {
-    console.error(err)
-  }
-}
+        return tx;
+    } catch (err) {
+        console.error(err);
+    }
+};
