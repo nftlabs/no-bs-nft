c56817483080f06aa05e1fe18ba289b215752110
nkrishang <kn1457@nyu.edu>
2021-05-04 10:52:05 -0400

    Added nft page

--- lib/magic.ts
+++ lib/magic.ts
@@ -4,14 +4,33 @@ import { isClient } from './isClient';
 import { RPCConnector } from './rpc_connector';
 
 export const magicServer = new MagicServer(process.env.NEXT_PUBLIC_MAGIC_SECRET_KEY);
-export const magicClient = isClient()
-    ? new MagicClient('pk_test_F7501FE7D4DD5B80', {
-          network: 'ropsten',
+
+export const MagicMaticClient = isClient()
+    ? new MagicClient(process.env.NEXT_PUBLIC_MAGIC_KEY, {
+          network: {
+              rpcUrl:
+                  'https://rpc-mainnet.maticvigil.com/v1/084e575b9401d628d1507747de3e0f72ef07261c/',
+              chainId: 137,
+          },
       })
     : null;
 
-export const magicRPCConnector = new RPCConnector({
-    rpcProvider: magicClient?.rpcProvider,
-    chainId: 3,
-});
+export const MagicMumbaiClient = isClient()
+    ? new MagicClient(process.env.NEXT_PUBLIC_MAGIC_KEY, {
+          network: {
+              rpcUrl:
+                  'https://rpc-mainnet.maticvigil.com/v1/084e575b9401d628d1507747de3e0f72ef07261c/',
+              chainId: 137,
+          },
+      })
+    : null;
 
+export const MagicRinkebyClient = isClient()
+    ? new MagicClient(process.env.NEXT_PUBLIC_MAGIC_KEY, {
+          network: {
+              rpcUrl:
+                  'https://rpc-mainnet.maticvigil.com/v1/084e575b9401d628d1507747de3e0f72ef07261c/',
+              chainId: 137,
+          },
+      })
+    : null;
--- pages/[username]/[collection]/[nft].tsx
+++ pages/[username]/[collection]/[nft].tsx
@@ -1,5 +1,40 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Magic } from 'magic-sdk';
+
+import { useWeb3React } from '@web3-react/core';
+import { Web3Provider } from '@ethersproject/providers';
+import { Contract } from '@ethersproject/contracts';
+import { Signer } from '@ethersproject/abstract-signer';
+
+import useUser from '../../../lib/useUser';
+import { MagicMaticClient } from '../../../lib/magic';
+import { putNFTOnSale, takeNFTOffSale } from '../../../lib/setSaleStatus';
+import buyNFT from '../../../lib/buyNFT';
 
 export default function NFT(): JSX.Element {
+    const [contract, setContract] = useState<Contract | null>(null);
+    const [signer, getSigner] = useState<Signer | null>(null);
+
+    const { user, login, logout } = useUser();
+
+    useEffect(() => {
+        
+    })
+
+    const handleNftBuy = async () => {
+        
+    }
+
+    const handlePutOnSale = async () => {
+        
+    }
+
+    const handleTakeOffSale = async () => {
+        
+    }
+
+    const handleChangePrice = async () => {
+        
+    }
     return <></>;
 }
